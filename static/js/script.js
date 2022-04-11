78eba77aa9694823d741aaa67ec778da557c795f
Luketedwards <Luketedwards96@hotmail.com>
2022-04-11 16:49:44 +0000

    Add task form input fields

--- static/js/script.js
+++ static/js/script.js
@@ -2,4 +2,12 @@ $(document).ready(function(){
     $('.sidenav').sidenav({edge: "right"});
     $('.tooltipped').tooltip();
     $('.collapsible').collapsible();
+    $('.datepicker').datepicker({
+      format: "dd mmmm, yyyy",
+      yearRange: 3,
+      showClearBtn: true,
+      i18n:{
+        done: 'Select'
+      }
+    });
   });
